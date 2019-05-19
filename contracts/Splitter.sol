ef3faf0b885ee1ec6c988ce2c0306edd604ea6f7
thetechrebel <stevedumbura@gmail.com>
2019-05-19 21:41:48 +0200

    Added Owned class to manage owner/admin rights

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,15 +1,38 @@
 pragma solidity ^0.5.0;
 
-contract Splitter{
+contract Owned{
+    //identifies the owner of the contract / admin
+    address internal owner;
+
+    //set the owner of the contract
+    constructor() public{
+        owner = msg.sender;
+    }
+
+    //a modifier that only the admin can do something
+    modifier  requireOwner() {
+        require(owner == msg.sender,"You are not the owner");
+        _;
+    }
+
+    //only the current owner should be able to change owner
+    function changeOwner(address newOwner) public requireOwner{
+        owner = newOwner;
+    }
+}
+
+contract Splitter is Owned{
     //the address of this contract
     address public splitterAddress;
+    address public Alice;
+    address public Carol;
+    address public Bob;
 
-    //when contract is created set the internal address variable
+
     constructor() public {
         splitterAddress = address(this);
     }
 
-    //get the balance of the splitter contract
     function getBalance() public view returns(uint balance){
         return splitterAddress.balance;
     }
